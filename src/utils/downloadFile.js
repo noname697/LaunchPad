545cf6f20269988d9092eb19048a47f1d7ba69c2
Arthur Manenti <arthur.manenti.videira@gmail.com>
2026-06-13 18:24:17 -0300

    Criando util para fazer o download dos files

---
+++ src/utils/downloadFile.js
@@ -0,0 +1,17 @@
+export const downloadFile = (content, fileName) => {
+  const blob = new Blob([content], {
+    type: "text/plain;charset=utf-8",
+  });
+
+  const url = URL.createObjectURL(blob);
+
+  const link = document.createElement("a");
+  link.href = url;
+  link.download = fileName;
+
+  document.body.appendChild(link);
+  link.click();
+
+  document.body.removeChild(link);
+  URL.revokeObjectURL(url);
+};
