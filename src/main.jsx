e50b4b3055fffebb52d7dcc750d675b7060be454
Arthur Manenti <arthur.manenti.videira@gmail.com>
2026-06-13 22:26:34 -0300

    Trocando browser router por hash router

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
+import { HashRouter } from "react-router";
 import App from "./App.jsx";
-import { BrowserRouter } from "react-router";
+import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 );
